dc0c0864ad71e68ded743300b4d2fc832bd88960
laynah.varnum98@gmail.com <lvarnum@email.arizona.edu>
2023-04-05 15:38:31 -0500

    create homepage

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,8 +1,23 @@
+import ProfilePic from '../../assets/me.jpg'
 import './Home.css'
 
 const HomePage = () => {
     return (
-        <div className="home-container"></div>
+        <div className="home-container">
+            <div id='hello-ribbon' className='intro-ribbon'>Hello</div>
+            <div id='name-ribbon' className='intro-ribbon'>My name is Laynah Varnum</div>
+            <div className='summary-container'>
+                <div className='summary'>
+                    <div className='title'>I am a <div className='job-title'>&nbsp;Front-end Developer</div></div>
+                    <div className='text'>
+                        I love creating clean, beautiful UI experiences that put the user first.
+                    </div>
+                    <div className='picture-container'>
+                        <img className='profile-pic' src={ProfilePic} alt='me'></img>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 

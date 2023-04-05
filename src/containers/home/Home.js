887c8e7101ca12f70dee937fdeb8a536c2bb9de4
Laynah Varnum <laynah.varnum98@gmail.com>
2023-04-05 16:42:15 -0500

    add social links

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,3 +1,4 @@
+import { AiOutlineGithub, AiFillLinkedin } from 'react-icons/ai'
 import ProfilePic from '../../assets/me.jpg'
 import './Home.css'
 
@@ -13,7 +14,13 @@ const HomePage = () => {
                         I love creating clean, beautiful UI experiences that put the user first.
                     </div>
                     <div className='picture-container'>
+                        <a className='link-icon' href='https://github.com/laynahwren' target="_blank" rel='noreferrer'>
+                            <AiOutlineGithub size={75} />
+                        </a>
                         <img className='profile-pic' src={ProfilePic} alt='me'></img>
+                        <a className='link-icon' href='https://www.linkedin.com/in/laynah-varnum-6a6ba3178/' target="_blank" rel='noreferrer'>
+                            <AiFillLinkedin size={75} />
+                        </a>
                     </div>
                 </div>
             </div>
